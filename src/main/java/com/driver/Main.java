7d6857830b2d543097ccd7a960f9804fcb248ab6
Naveenkumar7013 <telugunaveen55@gmail.com>
2023-09-12 23:39:17 +0530

    first attempt

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,29 @@
 package com.driver;
 
 public class Main {
+    public  static class Product{
+     public int product(int x,int y){
+
+         return 0;
+        }
+        public int product(int x,int y,int z){
+        return 0;
+        }
+        public double product(double x,double y){
+        return 0.0;
+        }
+        public static void main(String[] args) {
+            Product p = new Product();
+            p.product(2,3);
+            p.product(2,3,4);
+            p.product(2.3,3.4);
+        }
+
+        private void product() {
+        }
+    }
+
+
 
 }
+
